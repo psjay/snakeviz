827b2d993f4c06100efb467869135ef09139eeb2
PSJay <psjay.peng@gmail.com>
2016-01-31 16:36:53 +0800

    Make the webserver accept stats as dict in POST requests.

--- snakeviz/main.py
+++ snakeviz/main.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
+import logging
 import os.path
+import pickle
 from pstats import Stats
-
-try:
-    from urllib.parse import unquote_plus
-except ImportError:
-    from urllib import unquote_plus
 
 import tornado.ioloop
 import tornado.web
 
-from .stats import table_rows, json_stats
+from snakeviz.stats import table_rows, json_stats
 
 settings = {
     'static_path': os.path.join(os.path.dirname(__file__), 'static'),
@@ -21,19 +18,33 @@ settings = {
 }
 
 
+class DictSourceStats(Stats):
+    def load_stats(self, arg):
+        self.stats = arg
+
+
 class VizHandler(tornado.web.RequestHandler):
-    def get(self, profile_name):
-        profile_name = unquote_plus(profile_name)
+
+    def post(self):
+        content = self.get_argument('content')
+        s = None
+        if not content or not content.strip():
+            raise tornado.web.HTTPError(400)
         try:
-            s = Stats(profile_name)
-        except:
-            raise RuntimeError('Could not read %s.' % profile_name)
+            s = DictSourceStats(pickle.loads(content))
+        except Exception:
+            logging.warn('Load stats failed.', exc_info=True)
+            raise tornado.web.HTTPError(400)
+
+        if not isinstance(s, Stats):
+            raise tornado.web.HTTPError(400)
+
         self.render(
-            'viz.html', profile_name=profile_name,
+            'viz.html', profile_name='',
             table_rows=table_rows(s), callees=json_stats(s))
 
 
-handlers = [(r'/snakeviz/(.*)', VizHandler)]
+handlers = [(r'/', VizHandler)]
 
 app = tornado.web.Application(handlers, **settings)
 
